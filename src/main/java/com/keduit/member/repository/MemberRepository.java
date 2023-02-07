7056280e9f701a66c9ee1bb6187a10a6e3b0c80f
KEDUIT-PC\keduit <sdy9404@gmail.com>
2023-02-07 12:06:02 +0900

    member-test

---
+++ src/main/java/com/keduit/member/repository/MemberRepository.java
@@ -0,0 +1,5 @@
+package com.keduit.member.repository;
+
+public interface MemberRepository {
+
+}
---
+++ src/test/java/com/keduit/member/MemberTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keduit.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
